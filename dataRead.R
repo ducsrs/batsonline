820a061f1400d856a1bcf8dac3bd175fb8547531
Hallie Rutten <hallierutten@gmail.com>
2022-06-29 11:48:05 -0500

    moved intro data reading into sourced file

---
+++ dataRead.R
@@ -0,0 +1,20 @@
+# basic bat data read -----
+
+library(tidyverse)
+library(lubridate)
+library(readxl)
+
+bats <- readRDS('bats.RData')
+
+bats <- bats %>% 
+  mutate(year = year(DATE), 
+         monthN = month(DATE),
+         month = month.abb[monthN],
+         hour = hour(TIME),
+         sensor = paste(COMPARTMENT,SITE, sep='_') ) %>% 
+  filter( AUTO.ID != 'Noise' )
+
+# Any bat listed as CORROW should be listed as the secondary species (document)
+# Any bat listed as CORTOW should be listed as CORRAF (Amy text)
+bats <- bats %>% 
+  mutate( AUTO.ID = gsub("CORTOW", "CORRAF", AUTO.ID) )
