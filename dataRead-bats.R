94c38f671e9429fdc5070476a67320c0ab7cdfb9
Hallie Rutten <hallierutten@gmail.com>
2022-07-13 16:59:17 -0500

    minor edits

--- dataRead-bats.R
+++ dataRead-bats.R
@@ -19,5 +19,23 @@ bats <- bats %>%
          month = month.abb[monthN],
          hour = hour(TIME),
          sensor = paste(COMPARTMENT,SITE, sep='_'),
-         AUTO.ID = gsub("CORTOW", "CORRAF", AUTO.ID))
+         AUTO.ID = gsub("CORTOW", "CORRAF", AUTO.ID),
+         AUTO.ID = ifelse(grepl('noID',AUTO.ID,ignore.case=TRUE),'no.ID',AUTO.ID) )
 # Any bat listed as CORTOW should be listed as CORRAF (Amy text)
+
+# cave status column -----
+cave_obligate = c('MYOLEI','MYOAUS','MYOLUC','MYOSOD','MYOGRI','MYOSEP')
+seasonal_cave_obligate = c('PERSUB',"EPTFUS", "CORRAF")
+non_cave_obligate = c('LASBOR','NYCHUM','LASNOC','LASCIN')
+bats <- bats %>%
+  mutate( obligate = ifelse(AUTO.ID %in% cave_obligate, "cave obligate", AUTO.ID),
+          obligate = ifelse(AUTO.ID %in% non_cave_obligate, "not cave obligate", obligate),
+          obligate = ifelse(AUTO.ID %in% seasonal_cave_obligate, "seasonal cave obligate", obligate))
+
+# species group column -----
+EPTFUS.LASNOC = c("EPTFUS", "LASNOC")
+LASBOR.NYCHUM = c("LASBOR", "NYCHUM")
+bats <- bats %>% 
+  mutate(species_group = ifelse(grepl("^MYO", AUTO.ID), "MYOTIS", AUTO.ID),
+         species_group = ifelse(AUTO.ID %in% EPTFUS.LASNOC, "EPTFUS.LASNOC", species_group),
+         species_group = ifelse(AUTO.ID %in% LASBOR.NYCHUM, "LASBOR.NYCHUM", species_group))
